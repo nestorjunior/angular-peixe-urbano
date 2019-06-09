83e2c2d096924936d42dc231f85ba1dce996a673
nestorjunior <contato.nestor@gmail.com>
2019-06-08 22:49:50 -0300

    create routes items

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { FunComponent } from './fun/fun.component';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'fun', component: FunComponent },
+  { path: 'restaurants', component: RestaurantsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
