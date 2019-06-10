8ddf790ba45a942776919d47ee99621a27739661
nestorjunior <contato.nestor@gmail.com>
2019-06-10 16:33:10 -0300

    add snapshot route in component:sale

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { FunComponent } from './fun/fun.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { SaleComponent } from './sale/sale.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'fun', component: FunComponent },
-  { path: 'restaurants', component: RestaurantsComponent }
+  { path: 'restaurants', component: RestaurantsComponent },
+  { path: 'oferta', component: HomeComponent },
+  { path: 'oferta/:id', component: SaleComponent }
 ];
 
 @NgModule({
