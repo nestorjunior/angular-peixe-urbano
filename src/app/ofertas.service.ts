126e280a6ec8f2bfcbbe733ef1f82a9433d520ef
nestorjunior <contato.nestor@gmail.com>
2019-06-10 21:40:59 -0300

    ajuste URLAP

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -2,28 +2,30 @@ import { Http } from "@angular/http";
 import { Injectable } from "@angular/core";
 import { Oferta } from './shared/oferta.model';
 import { from } from 'rxjs';
+import { URL_API } from './app.api';
 
 @Injectable()
 export class OfertasService {
+
 
   constructor(private http: Http){}
 
   public getOfertas(): Promise<Oferta[]> {
     // Efetuar requisição http
-    return this.http.get('http://localhost:3000/ofertas?destaque=true')
+    return this.http.get(`${URL_API}?destaque=true`)
       .toPromise()
       .then((resposta: any) => resposta.json());
     // Retornar uma Promise de Oferta[]
   }
 
   public getOfertasCategoria(categoria: string): Promise<Oferta[]> {
-    return this.http.get(`http://localhost:3000/ofertas?categoria=${categoria}`)
+    return this.http.get(`${URL_API}?categoria=${categoria}`)
       .toPromise()
       .then((resposta: any) => resposta.json())
   }
 
   public getOfertasID(id: number): Promise<Oferta> {
-    return this.http.get(`http://localhost:3000/ofertas?id=${id}`)
+    return this.http.get(`${URL_API}?id=${id}`)
       .toPromise()
       .then((resposta: any) => {
         return resposta.json().shift();
