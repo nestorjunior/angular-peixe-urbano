ca23a42fb6cfe699b22651b54bc043aaa8e7719e
nestorjunior <contato.nestor@gmail.com>
2019-06-13 01:11:54 -0300

    include results search

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,6 +5,9 @@ import { Oferta } from '../shared/oferta.model';
 
 import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/of';
 
 @Component({
   selector: 'app-header',
@@ -22,10 +25,20 @@ export class HeaderComponent implements OnInit {
   ngOnInit() {
     this.ofertas = this.itemPesquisa
       .debounceTime(1000) // executa a ação do switchMap após 1 segundo
+      .distinctUntilChanged()
       .switchMap((termo: string) => {
         console.log('requisição http para api')
+
+        if(termo.trim() === '') {
+          // retornar um Observable de array de ofertas vazio
+          return Observable.of<Oferta[]>([])
+        }
         return this.ofertasServ.pesquisaOfertas(termo)
       })
+      .catch((err: any) => {
+        console.log(err)
+        return Observable.of<Oferta[]>([])
+      });
 
     this.ofertas.subscribe((ofertas: Oferta[]) => console.log(ofertas))
   }
