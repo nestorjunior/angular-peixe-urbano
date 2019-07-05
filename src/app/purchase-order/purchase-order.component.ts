e3e97190ad544b06883e79d37672611c5f4fdfd0
nestorjunior <contato.nestor@gmail.com>
2019-07-05 00:48:37 -0300

    refatorando: purchase-order

--- src/app/purchase-order/purchase-order.component.ts
+++ src/app/purchase-order/purchase-order.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { OrdemCompraService } from '../ordem-compra.service';
 import { Pedido } from '../shared/pedido.model';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-purchase-order',
@@ -11,12 +11,14 @@ import { FormGroup, FormControl } from '@angular/forms';
 })
 export class PurchaseOrderComponent implements OnInit {
 
+  public idPedidoCompra: number
+
   public formulario: FormGroup = new FormGroup({
-    'endereco': new FormControl(null),
-    'numero': new FormControl(null),
+    'endereco': new FormControl(null, [ Validators.required, Validators.minLength(3), Validators.maxLength(120) ]),
+    'numero': new FormControl(null, [ Validators.required, Validators.minLength(1), Validators.maxLength(20) ]),
     'complemento': new FormControl(null),
-    'formaPagamento': new FormControl(null)
-  })
+    'formaPagamento': new FormControl(null, [ Validators.required ])
+  });
 
   constructor(private ordemCompras: OrdemCompraService) { }
 
@@ -25,7 +27,18 @@ export class PurchaseOrderComponent implements OnInit {
   }
 
   public confirmarCompra(): void {
+    let pedido: Pedido = new Pedido (
+      this.formulario.value.endereco,
+      this.formulario.value.numero,
+      this.formulario.value.complemento,
+      this.formulario.value.formaPagamento
+    )
 
+    this.ordemCompras.efetivarCompra(pedido)
+      .subscribe((idPedido: number) => {
+        this.idPedidoCompra = idPedido
+        console.log(this.idPedidoCompra)
+      })
   }
 
 }
